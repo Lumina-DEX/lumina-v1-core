ee3d780aefa0d241563ed499826d8a9d3df003dc
Youtpout <EddyBoughioul@hotmail.fr>
2024-07-11 15:30:52 +0200

    add example of pool for mina support

---
+++ contracts/src/PoolMina.old.ts
@@ -0,0 +1,180 @@
+import { Account, AccountUpdate, Bool, Field, Mina, PrivateKey, PublicKey, TokenContract, UInt64 } from 'o1js';
+
+import { Pool, minimunLiquidity } from './Pool';
+import { TestToken } from './TestToken';
+import { TokenHolder } from './TokenHolder';
+
+let proofsEnabled = false;
+
+describe('Pool Mina', () => {
+  let deployerAccount: Mina.TestPublicKey,
+    deployerKey: PrivateKey,
+    senderAccount: Mina.TestPublicKey,
+    senderKey: PrivateKey,
+    bobAccount: Mina.TestPublicKey,
+    bobKey: PrivateKey,
+    aliceAccount: Mina.TestPublicKey,
+    aliceKey: PrivateKey,
+    zkAppAddress: PublicKey,
+    zkAppPrivateKey: PrivateKey,
+    zkApp: Pool,
+    zkToken0Address: PublicKey,
+    zkToken0PrivateKey: PrivateKey,
+    zkToken0: TestToken,
+    tokenHolder0: TokenHolder,
+    tokenHolder1: TokenHolder;
+
+  beforeEach(async () => {
+    const Local = await Mina.LocalBlockchain({ proofsEnabled });
+    Mina.setActiveInstance(Local);
+    [deployerAccount, senderAccount, bobAccount, aliceAccount] = Local.testAccounts;
+    deployerKey = deployerAccount.key;
+    senderKey = senderAccount.key;
+    bobKey = bobAccount.key;
+    aliceKey = aliceAccount.key;
+
+    zkAppPrivateKey = PrivateKey.random();
+    zkAppAddress = zkAppPrivateKey.toPublicKey();
+    zkApp = new Pool(zkAppAddress);
+
+    zkToken0PrivateKey = PrivateKey.random();
+    zkToken0Address = zkToken0PrivateKey.toPublicKey();
+    zkToken0 = new TestToken(zkToken0Address);
+
+    if (proofsEnabled) {
+      console.time('compile token');
+      const tokenKey = await TestToken.compile();
+      console.timeEnd('compile token');
+      console.time('compile pool');
+      const key = await Pool.compile();
+      console.timeEnd('compile pool');
+      console.log("key pool", key.verificationKey.data);
+      console.log("key pool hash", key.verificationKey.hash.toBigInt());
+    }
+
+
+    const txn = await Mina.transaction(deployerAccount, async () => {
+      AccountUpdate.fundNewAccount(deployerAccount, 2);
+      await zkApp.deploy();
+      await zkToken0.deploy();
+    });
+    await txn.prove();
+    // this tx needs .sign(), because `deploy()` adds an account update that requires signature authorization
+    await txn.sign([deployerKey, zkAppPrivateKey, zkToken0PrivateKey]).send();
+
+    tokenHolder0 = new TokenHolder(zkAppAddress, zkToken0.deriveTokenId());
+    tokenHolder1 = new TokenHolder(zkAppAddress, Field.from(0n));
+
+    const txn2 = await Mina.transaction(deployerAccount, async () => {
+      AccountUpdate.fundNewAccount(deployerAccount, 2);
+      await tokenHolder0.deploy();
+      await tokenHolder1.deploy();
+      await zkToken0.approveAccountUpdate(tokenHolder0.self);
+    });
+    await txn2.prove();
+    // this tx needs .sign(), because `deploy()` adds an account update that requires signature authorization
+    await txn2.sign([deployerKey, zkAppPrivateKey]).send();
+
+    // mint token to user
+    await mintToken(senderAccount);
+
+  });
+
+  it('create pool', async () => {
+    showBalanceToken0();
+    showBalanceToken1();
+
+    let amt = UInt64.from(10 * 10 ** 9);
+    const txn = await Mina.transaction(senderAccount, async () => {
+      AccountUpdate.fundNewAccount(senderAccount, 1);
+      await zkApp.createPool(zkToken0Address, PublicKey.empty(), amt, amt);
+    });
+    await txn.prove();
+    await txn.sign([senderKey]).send();
+
+    showBalanceToken0();
+    showBalanceToken1();
+
+    const liquidityUser = Mina.getBalance(senderAccount, zkApp.deriveTokenId());
+    const expected = amt.value.add(amt.value).sub(minimunLiquidity.value);
+    console.log("liquidity user", liquidityUser.toString());
+    expect(liquidityUser.value).toEqual(expected);
+  });
+
+  it('swap tokens', async () => {
+    let amt = UInt64.from(10 * 10 ** 9);
+    const txn = await Mina.transaction(senderAccount, async () => {
+      AccountUpdate.fundNewAccount(senderAccount, 1);
+      await zkApp.createPool(zkToken0Address, PublicKey.empty(), amt, amt);
+    });
+    await txn.prove();
+    await txn.sign([senderKey]).send();
+
+    showBalanceToken0();
+    showBalanceToken1();
+    let amtSwap = UInt64.from(1 * 10 ** 9);
+    const txn2 = await Mina.transaction(senderAccount, async () => {
+      // AccountUpdate.fundNewAccount(senderAccount, 1);
+      await zkApp.swapExactAmountIn(zkToken0Address, amtSwap, UInt64.from(1));
+    });
+    await txn2.prove();
+    await txn2.sign([senderKey]).send();
+
+    showBalanceToken0();
+    showBalanceToken1();
+  });
+
+  it('supply liquidity', async () => {
+
+    await mintToken(bobAccount);
+
+    let amt = UInt64.from(10 * 10 ** 9);
+    const txn = await Mina.transaction(senderAccount, async () => {
+      AccountUpdate.fundNewAccount(senderAccount, 1);
+      await zkApp.createPool(zkToken0Address, PublicKey.empty(), amt, amt);
+    });
+    await txn.prove();
+    await txn.sign([senderKey]).send();
+
+    showBalanceToken0();
+    showBalanceToken1();
+    let amtLiquidity = UInt64.from(1 * 10 ** 9);
+    const txn2 = await Mina.transaction(bobAccount, async () => {
+      AccountUpdate.fundNewAccount(bobAccount, 1);
+      await zkApp.supplyLiquidity(zkToken0Address, amtLiquidity, amtLiquidity.mul(2));
+    });
+    await txn2.prove();
+    await txn2.sign([bobKey]).send();
+
+    showBalanceToken0();
+    showBalanceToken1();
+
+    const liquidityUser = Mina.getBalance(bobAccount, zkApp.deriveTokenId());
+    console.log("liquidity bob", liquidityUser.toString());
+  });
+
+
+  function showBalanceToken0() {
+    let bal = Mina.getBalance(senderAccount, zkToken0.deriveTokenId());
+    console.log("balance user 0", bal.toBigInt());
+    return bal;
+  }
+
+  function showBalanceToken1() {
+    let bal = Mina.getBalance(senderAccount);
+    console.log("balance user 1", bal.toBigInt());
+    return bal;
+  }
+
+  async function mintToken(user: PublicKey) {
+    // update transaction
+    const txn = await Mina.transaction(senderAccount, async () => {
+      AccountUpdate.fundNewAccount(senderAccount, 1);
+      await zkToken0.mintTo(user, UInt64.from(1000 * 10 ** 9));
+    });
+    await txn.prove();
+    await txn.sign([senderKey, zkToken0PrivateKey]).send();
+
+  }
+
+});
---
+++ contracts/src/PoolMina.ts
@@ -0,0 +1,159 @@
+import { Field, SmartContract, state, State, method, TokenContract, PublicKey, AccountUpdateForest, DeployArgs, UInt64, AccountUpdate, Provable } from 'o1js';
+import { TokenStandard } from './TokenStandard';
+import { TokenHolder } from './TokenHolder';
+
+// minimum liquidity permanently locked in the pool
+export const minimunLiquidity: UInt64 = new UInt64(10 ** 3);
+
+/**
+ * Pool contract for Lumina dex (Future implementation for direct mina token support)
+ */
+export class PoolMina extends TokenContract {
+    // we need the token address to instantiate it
+    @state(PublicKey) tokenA = State<PublicKey>();
+    @state(UInt64) liquiditySupply = State<UInt64>();
+
+    init() {
+        super.init();
+    }
+
+    @method async approveBase(forest: AccountUpdateForest) {
+        this.checkZeroBalanceChange(forest);
+    }
+
+    @method async createPool(tokenA: PublicKey, amountA: UInt64, amountMina: UInt64) {
+        const addressA = this.tokenA.getAndRequireEquals();
+
+        addressA.isEmpty().assertTrue("Pool already initialised");
+
+        amountA.assertGreaterThan(UInt64.zero, "No amount A supplied");
+        amountMina.assertGreaterThan(UInt64.zero, "No amount Mina supplied");
+
+        let tokenContractA = new TokenStandard(tokenA);
+
+        // require signature on transfer, so don't need to request it now
+        let sender = this.sender.getAndRequireSignature();
+        let senderUpdate = AccountUpdate.createSigned(sender);
+
+        // https://docs.openzeppelin.com/contracts/4.x/erc4626#inflation-attack, check if necessary in our case
+        amountA.add(amountMina).assertGreaterThan(minimunLiquidity, "Insufficient amount to mint liquidities");
+
+        await tokenContractA.transfer(sender, this.address, amountA);
+        await senderUpdate.send({ to: this.address, amount: amountMina });
+
+        // calculate liquidity token output simply as liquidityAmount = amountA + amountB - minimal liquidity, todo check overflow  
+        // => maintains ratio a/l, b/l
+        let liquidityAmount = amountA.add(amountMina).sub(minimunLiquidity);
+        // mint token
+        this.internal.mint({ address: sender, amount: liquidityAmount });
+
+        // set default informations
+        this.tokenA.set(tokenA);
+        this.liquiditySupply.set(liquidityAmount.add(minimunLiquidity));
+    }
+
+    @method async supplyLiquidityFromTokenA(amountA: UInt64, maxAmountMina: UInt64) {
+        amountA.assertGreaterThan(UInt64.zero, "No amount A supplied");
+
+        const addressA = this.tokenA.getAndRequireEquals();
+
+        // todo manage mina native token
+        addressA.isEmpty().assertFalse("Pool not initialised");
+
+        let tokenContractA = new TokenStandard(addressA);
+
+        const holderA = new TokenHolder(this.address, tokenContractA.deriveTokenId());
+        const holderB = new TokenHolder(this.address);
+
+        const balanceA = holderA.account.balance.getAndRequireEquals();
+        const balanceB = holderB.account.balance.getAndRequireEquals();
+
+        // amount Y to supply
+        const amountMina = amountA.mul(balanceB).div(balanceA);
+
+        amountMina.assertGreaterThan(UInt64.zero, "No Mina amount to supply");
+        amountMina.assertLessThanOrEqual(maxAmountMina, "Mina amount greater than desired amount");
+
+        // require signature on transfer, so don't need to request it now
+        let sender = this.sender.getUnconstrained();
+        let senderUpdate = AccountUpdate.createSigned(sender);
+
+        await tokenContractA.transfer(sender, this.address, amountA);
+        await senderUpdate.send({ to: this.address, amount: amountMina });
+
+        const actualSupply = this.liquiditySupply.getAndRequireEquals();
+
+        // calculate liquidity token output simply as liquidityAmount = amountA + amountB 
+        // => maintains ratio a/l, b/l
+        let liquidityAmount = amountA.add(amountMina);
+        // mint token
+        this.internal.mint({ address: sender, amount: liquidityAmount });
+
+        // set new supply
+        this.liquiditySupply.set(actualSupply.add(liquidityAmount));
+    }
+
+    @method async supplyLiquidityFromMina(amountMina: UInt64, maxAmountA: UInt64) {
+        amountMina.assertGreaterThan(UInt64.zero, "No Mina amount supplied");
+
+        const addressA = this.tokenA.getAndRequireEquals();
+
+        // todo manage mina native token
+        addressA.isEmpty().assertFalse("Pool not initialised");
+
+        let tokenContractA = new TokenStandard(addressA);
+
+        const holderA = new TokenHolder(this.address, tokenContractA.deriveTokenId());
+        const holderB = new TokenHolder(this.address);
+
+        const balanceA = holderA.account.balance.getAndRequireEquals();
+        const balanceB = holderB.account.balance.getAndRequireEquals();
+
+        // amount Y to supply
+        const amountA = amountMina.mul(balanceA).div(balanceB);
+
+        amountA.assertGreaterThan(UInt64.zero, "No amount A to supply");
+        amountA.assertLessThanOrEqual(maxAmountA, "Amount A greater than desired amount");
+
+        // require signature on transfer, so don't need to request it now
+        let sender = this.sender.getUnconstrained();
+        let senderUpdate = AccountUpdate.createSigned(sender);
+
+        await tokenContractA.transfer(sender, this.address, amountA);
+        await senderUpdate.send({ to: this.address, amount: amountMina });
+
+        const actualSupply = this.liquiditySupply.getAndRequireEquals();
+
+        // calculate liquidity token output simply as liquidityAmount = amountA + amountB 
+        // => maintains ratio a/l, b/l
+        let liquidityAmount = amountA.add(amountMina);
+        // mint token
+        this.internal.mint({ address: sender, amount: liquidityAmount });
+
+        // set new supply
+        this.liquiditySupply.set(actualSupply.add(liquidityAmount));
+    }
+
+    // @method async swapExactAmountIn(amountIn: UInt64, amountOutMin: UInt64) {
+    //     amountIn.assertGreaterThan(UInt64.zero, "No amount in supplied");
+    //     amountOutMin.assertGreaterThan(UInt64.zero, "No amount out supplied");
+
+    //     const addressA = this.tokenA.getAndRequireEquals();
+
+    //     // todo manage mina native token
+    //     addressA.isEmpty().assertFalse("Pool not initialised");
+
+    //     // we request token out because this is the token holder who update his balance to transfer out
+    //     let tokenOut = Provable.if(tokenIn.equals(addressA), addressB, addressA);
+    //     let tokenContractOut = new TokenStandard(tokenOut);
+    //     let tokenHolderOut = new TokenHolder(this.address, tokenContractOut.deriveTokenId());
+
+    //     // require signature on transfer, so don't need to request it now
+    //     let sender = this.sender.getUnconstrained();
+
+    //     // will transfer token in to this pool and calculate correct amount out to transfer the token out
+    //     const amountOut = await tokenHolderOut.swap(this.address, sender, amountIn, amountOutMin);
+    //     await tokenContractOut.transfer(tokenHolderOut.self, sender, amountOut);
+    // }
+
+}
