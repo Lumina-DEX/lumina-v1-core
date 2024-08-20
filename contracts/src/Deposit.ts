9ad7503df5ba7d4a94a99ae0bb0e7b2b43a64c74
eddy <be@be-technology.fr>
2024-08-20 08:56:29 +0200

    add deposit and withdraw contracts

---
+++ contracts/src/Deposit.ts
@@ -0,0 +1,44 @@
+import { Field, SmartContract, state, State, method, TokenContract, PublicKey, AccountUpdateForest, DeployArgs, UInt64, AccountUpdate, Provable } from 'o1js';
+import { TokenStandard } from './index.js';
+import { Withdraw } from './Withdraw.js';
+
+
+/**
+ * Pool contract for Lumina dex
+ */
+export class Deposit extends SmartContract {
+    @state(PublicKey) approvedKey = State<PublicKey>();
+
+
+    init() {
+        super.init();
+    }
+
+    @method async deposit(tokenA: PublicKey, tokenB: PublicKey, amountA: UInt64, amountB: UInt64) {
+
+        let tokenContractA = new TokenStandard(tokenA);
+        let tokenContractB = new TokenStandard(tokenA);
+
+        const sender = this.sender.getUnconstrained();
+
+        await tokenContractA.transfer(this.sender.getUnconstrained(), this.address, amountA);
+        await tokenContractB.transfer(this.sender.getUnconstrained(), this.address, amountB);
+    }
+
+    @method async withdraw(hashProof: string, nonce: UInt64, tokenA: PublicKey, tokenB: PublicKey, amountA: UInt64, amountB: UInt64, receiver: PublicKey) {
+
+        let tokenContractA = new TokenStandard(tokenA);
+        let tokenContractB = new TokenStandard(tokenA);
+
+        const sender = this.sender.getAndRequireSignature();
+
+        let senderContract = new TokenStandard(sender);
+
+        let withdrawContractSender = new Withdraw(this.address, senderContract.deriveTokenId());
+        withdrawContractSender.addProof()
+
+        await tokenContractA.transfer(this.self, receiver, amountA);
+        await tokenContractB.transfer(this.self, receiver, amountB);
+    }
+
+}
---
+++ contracts/src/Withdraw.ts
@@ -0,0 +1,31 @@
+import { Field, SmartContract, state, State, method, TokenContract, PublicKey, AccountUpdateForest, DeployArgs, UInt64, AccountUpdate, Provable, Bool, assert } from 'o1js';
+import { TokenStandard } from './index.js';
+
+
+/**
+ * Pool contract for Lumina dex
+ */
+export class Withdraw extends SmartContract {
+
+    @state(UInt64) lastNonce = State<UInt64>();
+
+    init() {
+        super.init();
+    }
+
+    @method.returns(Bool) async addProof(hashProof: string, nonce: UInt64, tokenA: PublicKey, tokenB: PublicKey, amountA: UInt64, amountB: UInt64, receiver: PublicKey) {
+
+        // check proof
+        let actualNonce = this.lastNonce.getAndRequireEquals();
+        let expectedNonce = actualNonce.add(1);
+
+        expectedNonce.assertEquals(nonce, "Incorrect proof")
+
+        this.self.body.mayUseToken = AccountUpdate.MayUseToken.ParentsOwnToken;
+
+        this.lastNonce.set(expectedNonce);
+
+        return Bool(true);
+    }
+
+}
