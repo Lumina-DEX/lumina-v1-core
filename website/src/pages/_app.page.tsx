29032173e829a78fad9ce1d4065d9f0630721428
Youtpout <EddyBoughioul@hotmail.fr>
2024-09-12 13:05:13 +0200

    correct filename

--- website/src/pages/_app.page.tsx
+++ website/src/pages/_app.page.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import './reactCOIServiceWorker';
-import ErrorBoundary from '@/components/errorBoundary';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 export default function App({ Component, pageProps }: AppProps) {
 
