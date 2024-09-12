64d99e70359df3f4fa3bce55238fada04b433edd
Youtpout <EddyBoughioul@hotmail.fr>
2024-09-12 12:56:16 +0200

    add error boundary

--- website/src/pages/_app.page.tsx
+++ website/src/pages/_app.page.tsx
@@ -1,8 +1,11 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-
 import './reactCOIServiceWorker';
+import ErrorBoundary from '@/components/errorBoundary';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+  return (<ErrorBoundary>
+    <Component {...pageProps} />
+  </ErrorBoundary>);
 }
