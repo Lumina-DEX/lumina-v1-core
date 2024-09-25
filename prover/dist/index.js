15515ba1b9edb085bd8450edb08fa88c71cd980c
Youtpout <EddyBoughioul@hotmail.fr>
2024-09-25 20:34:37 +0200

    try to use worker

--- prover/dist/index.js
+++ prover/dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
-const swap_1 = require("./api/swap");
+const prove_1 = require("./api/prove");
 // configures dotenv to work in your application
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -24,15 +15,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express_1.default.json());
 // enabling CORS for all requests
 app.use((0, cors_1.default)());
-app.use("/api/swap", swap_1.swapRouter);
+app.use("/api/swap", prove_1.proveRouter);
 // added root redirect
-app.get(`/`, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get(`/`, async (req, res) => {
     res.redirect('/api');
-}));
+});
 // added the api route
-app.get(`/api`, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get(`/api`, async (req, res) => {
     res.json({ up: true });
-}));
+});
 app.listen(PORT, () => {
     console.log("Server running at PORT: ", PORT);
 }).on("error", (error) => {
