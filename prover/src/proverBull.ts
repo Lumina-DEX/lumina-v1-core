8abc1e63436d68c33a0171d44c104270a5dbd4a8
Youtpout <EddyBoughioul@hotmail.fr>
2024-09-27 19:02:24 +0200

    try to use sandbox

--- prover/src/proverBull.ts
+++ prover/src/proverBull.ts
@@ -1,8 +1,11 @@
 import { Queue, Worker, QueueEvents } from 'bullmq';
 import IORedis from 'ioredis';
+import { fileURLToPath } from 'url';
 
 import { AccountUpdate, Cache, fetchAccount, Mina, PublicKey, UInt64 } from 'o1js';
 import { PoolMina, PoolTokenHolder, FungibleToken, FungibleTokenAdmin } from "../dist/src/contracts/indexmina.js";
+import path from 'path';
+import sandbox from './sandbox';
 
 const connection = new IORedis({ maxRetriesPerRequest: null });
 
@@ -27,47 +30,11 @@ export async function addJobs() {
 }
 
 
+// const filename = fileURLToPath(import.meta.url);
 
-console.time("compile");
-//const cacheFiles = await fetchFromServerFiles();
-const cache = Cache.FileSystem("./cache");
-console.log("compile pool mina");
-await PoolMina.compile({ cache })
-console.log("compile pool token holder");
-await PoolTokenHolder.compile({ cache });
-console.log("compile pool token admin");
-await FungibleTokenAdmin.compile({ cache });
-console.log("compile pool fungible token");
-await FungibleToken.compile({ cache });
-console.timeEnd("compile");
+// const dirname = path.dirname(filename);
+// console.log("__dirname", dirname);
+// const processorFile = path.join(dirname, "sandbox.ts");
+// console.log("file", processorFile);
 
-
-new Worker('swapFromMina', async (job: any) => {
-
-    const poolKey = PublicKey.fromBase58("B62qjmz2oEe8ooqBmvj3a6fAbemfhk61rjxTYmUMP9A6LPdsBLmRAxK");
-
-    console.log("fetch");
-    const acc = await fetchAccount({ publicKey: poolKey });
-    console.log("acc", acc.account.balance.toBigInt());
-
-    console.log("pool");
-    const pool = new PoolMina(poolKey)
-    const token = await pool.token.getAndRequireEquals();
-    console.log("token", token.toBase58());
-
-    const frontend = PublicKey.fromBase58("B62qoSZbMLJSP7dHLqe8spFPFSsUoENnMSHJN8i5bS1X4tdGpAZuwAC");
-
-    console.time("prove");
-    let amt = UInt64.from(10 * 10 ** 9);
-    let amtToken = UInt64.from(50 * 10 ** 9);
-    let txn = await Mina.transaction(frontend, async () => {
-        AccountUpdate.fundNewAccount(frontend, 2);
-        await pool.supplyFirstLiquidities(amt, amtToken);
-    });
-
-    const tx = await txn.prove();
-    console.timeEnd("prove");
-
-    return { token: token.toBase58(), transaction: tx.toJSON() };
-
-}, { connection });
+new Worker('swapFromMina', sandbox, { connection, useWorkerThreads: true });
