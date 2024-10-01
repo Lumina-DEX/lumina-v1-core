adf5ec528921edd7f6f2393c0f7b97ab4d57c29b
Youtpout <EddyBoughioul@hotmail.fr>
2024-10-01 14:32:30 +0200

    dynamic loading of token

--- website/src/components/TokenMenu.tsx
+++ website/src/components/TokenMenu.tsx
@@ -1,17 +1,22 @@
 "use client";
 import React, { useEffect, useMemo, useState } from "react";
 import { Addresses } from "@/utils/addresses";
+import useAccount from "@/states/useAccount";
 
 const TokenMenu = ({ pool, setPool }) => {
 
   const [tokenList, setTokenList] = useState([]);
+  const accountState = useAccount();
+
   useEffect(() => {
     console.log("token", pool);
+    console.log("accountState update");
     Addresses.getList().then((x: any) => {
-      const tokens = x?.tokens?.filter(z => z.chainId === "mina-devnet");
+      const network = accountState.network === "mina:testnet" ? "mina-devnet" : "zeko-devnet";
+      const tokens = x?.tokens?.filter(z => z.chainId === network);
       setTokenList(tokens)
     });
-  }, [])
+  }, [accountState.network])
 
   return (<select className="ml-3" value={pool} onChange={async (ev) => await setPool(ev.target.value)}>
     {tokenList.map(x => <option key={x.poolAddress} title={x.name} value={x.poolAddress}>{x.symbol}</option>)}
