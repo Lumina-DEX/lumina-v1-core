4f55c9ac0d1030170c38009dc1899451aaf67b70
Youtpout <EddyBoughioul@hotmail.fr>
2024-11-22 16:53:07 +0100

    remove unuse prover

--- prover/src/contracts/indexmina.ts
+++
@@ -1,7 +0,0 @@
-
-export * from './MathLibrary.js';
-export * from 'mina-fungible-token';
-export * from './PoolMina.js';
-export * from "./Faucet.js";
-export * from "./PoolTokenHolder.js";
-export * from "./PoolFactory.js";
