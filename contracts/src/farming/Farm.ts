058504f6957f493a5702138a3b45cb2f19b28b20
Youtpout <EddyBoughioul@hotmail.fr>
2024-12-13 12:02:41 +0100

    add new farm contracts

--- contracts/src/farming/Farm.ts
+++ contracts/src/farming/Farm.ts
@@ -1,155 +1,156 @@
-import { Field, Permissions, state, State, method, TokenContractV2, PublicKey, AccountUpdateForest, DeployArgs, UInt64, AccountUpdate, Provable, VerificationKey, Bool, Int64, assert, Types } from 'o1js';
-import { BalanceChangeEvent } from '../indexpool.js';
-import { FarmStorage } from './FarmStorage';
+import {
+  Account,
+  AccountUpdate,
+  AccountUpdateForest,
+  assert,
+  Bool,
+  CircuitString,
+  DeployArgs,
+  Field,
+  Int64,
+  method,
+  Permissions,
+  Provable,
+  PublicKey,
+  Reducer,
+  SmartContract,
+  State,
+  state,
+  Struct,
+  TokenContractV2,
+  TokenId,
+  Types,
+  UInt32,
+  UInt64,
+  VerificationKey
+} from "o1js"
+import { BalanceChangeEvent, mulDiv, Pool, PoolTokenHolder } from "../indexpool.js"
 
-export interface FarmingDeployProps extends Exclude<DeployArgs, undefined> {
-    pool: PublicKey;
-    owner: PublicKey;
+export class FarmingInfo extends Struct({
+  tokenAddress: PublicKey,
+  startTimestamp: UInt64,
+  endTimestamp: UInt64,
+  root: Field
+}) {
+  constructor(value: {
+    tokenAddress: PublicKey
+    totalReward: UInt64
+    startTimestamp: UInt64
+    endTimestamp: UInt64
+    root: Field
+  }) {
+    super(value)
+  }
 }
 
+export class FarmingEvent extends Struct({
+  sender: PublicKey,
+  amount: UInt64
+}) {
+  constructor(value: {
+    sender: PublicKey
+    amount: UInt64
+  }) {
+    super(value)
+  }
+}
+
+export interface FarmingDeployProps extends Exclude<DeployArgs, undefined> {
+  pool: PublicKey
+  owner: PublicKey
+  rewardAddress: PublicKey
+  farmingInfo: FarmingInfo
+}
 
 /**
  * Farm contract
  */
 export class Farm extends TokenContractV2 {
-    // Farming for one pool
-    @state(PublicKey) pool = State<PublicKey>();
-    @state(PublicKey) owner = State<PublicKey>();
+  // Farming for one pool
+  @state(PublicKey)
+  pool = State<PublicKey>()
+  @state(PublicKey)
+  owner = State<PublicKey>()
+  @state(FarmingInfo)
+  farmingInfo = State<FarmingInfo>()
 
+  events = {
+    upgrade: Field,
+    deposit: FarmingEvent,
+    withdraw: FarmingEvent
+  }
 
-    events = {
-        upgrade: Field,
-        BalanceChange: BalanceChangeEvent,
-    };
+  async deploy(args: FarmingDeployProps) {
+    await super.deploy()
 
-    async deploy(args: FarmingDeployProps) {
-        await super.deploy();
+    args.pool.isEmpty().assertFalse("Pool empty")
+    args.owner.isEmpty().assertFalse("Owner empty")
 
-        args.pool.isEmpty().assertFalse("Pool empty");
-        args.owner.isEmpty().assertFalse("Owner empty");
+    const currentTimestamp = this.network.timestamp.getAndRequireEquals()
+    args.farmingInfo.startTimestamp.assertGreaterThanOrEqual(
+      currentTimestamp,
+      "Start timestamp need to be greater or equal to the current timestamp"
+    )
+    args.farmingInfo.endTimestamp.assertGreaterThan(
+      currentTimestamp,
+      "End timestamp need to be greater than current timestamp"
+    )
 
-        this.pool.set(args.pool);
-        this.owner.set(args.owner);
+    this.pool.set(args.pool)
+    this.owner.set(args.owner)
+    this.farmingInfo.set(args.farmingInfo)
 
-        let permissions = Permissions.default();
-        permissions.access = Permissions.proofOrSignature();
-        permissions.setPermissions = Permissions.impossible();
-        permissions.setVerificationKey = Permissions.VerificationKey.proofDuringCurrentVersion();
-        this.account.permissions.set(permissions);
-    }
+    let permissions = Permissions.default()
+    permissions.access = Permissions.proof()
+    permissions.setPermissions = Permissions.impossible()
+    permissions.setVerificationKey = Permissions.VerificationKey.impossibleDuringCurrentVersion()
+    this.account.permissions.set(permissions)
+  }
 
-    /**
-    * Upgrade to a new version
-    * @param vk new verification key
-    */
-    @method async updateVerificationKey(vk: VerificationKey) {
-        const owner = await this.owner.getAndRequireEquals();
+  /** Approve `AccountUpdate`s that have been created outside of the token contract.
+   *
+   * @argument {AccountUpdateForest} updates - The `AccountUpdate`s to approve. Note that the forest size is limited by the base token contract, @see TokenContractV2.MAX_ACCOUNT_UPDATES The current limit is 9.
+   */
+  @method
+  async approveBase(updates: AccountUpdateForest): Promise<void> {
+    Bool(false).assertTrue("You can't manage the token")
+  }
 
-        // only owner can update a pool
-        AccountUpdate.createSigned(owner);
+  /**
+   * Upgrade to a new version
+   * @param vk new verification key
+   */
+  @method
+  async updateVerificationKey(vk: VerificationKey) {
+    const owner = await this.owner.getAndRequireEquals()
 
-        this.account.verificationKey.set(vk);
-        this.emitEvent("upgrade", vk.hash);
-    }
+    // only owner can update a pool
+    AccountUpdate.createSigned(owner)
 
-    /** Approve `AccountUpdate`s that have been created outside of the token contract.
-      *
-      * @argument {AccountUpdateForest} updates - The `AccountUpdate`s to approve. Note that the forest size is limited by the base token contract, @see TokenContractV2.MAX_ACCOUNT_UPDATES The current limit is 9.
-      */
-    @method
-    async approveBase(updates: AccountUpdateForest): Promise<void> {
-        let totalBalance = Int64.from(0)
-        this.forEachUpdate(updates, (update, usesToken) => {
-            // Make sure that the account permissions are not changed
-            this.checkPermissionsUpdate(update)
-            this.emitEventIf(
-                usesToken,
-                "BalanceChange",
-                new BalanceChangeEvent({ address: update.publicKey, amount: update.balanceChange }),
-            )
+    this.account.verificationKey.set(vk)
+    this.emitEvent("upgrade", vk.hash)
+  }
 
-            // Don't allow transfers to/from the account that's tracking circulation
-            update.publicKey.equals(this.address).and(usesToken).assertFalse(
-                "Can't transfer to/from the circulation account"
-            )
-            totalBalance = Provable.if(usesToken, totalBalance.add(update.balanceChange), totalBalance)
-            totalBalance.isPositiveV2().assertFalse(
-                "Flash-minting or unbalanced transaction detected"
-            )
-        })
-        totalBalance.assertEquals(Int64.zero, "Unbalanced transaction")
-    }
+  @method
+  async deposit(amount: UInt64) {
+    const farmingInfo = this.farmingInfo.getAndRequireEquals()
+    // user can deposit only during this period
+    this.network.timestamp.requireBetween(farmingInfo.startTimestamp, farmingInfo.endTimestamp)
 
-    private checkPermissionsUpdate(update: AccountUpdate) {
-        let permissions = update.update.permissions
+    const poolAddress = this.pool.getAndRequireEquals()
+    const pool = new Pool(poolAddress)
+    const sender = this.sender.getUnconstrainedV2()
+    // transfer amount to this account
+    await pool.transfer(sender, this.address, amount)
+    this.internal.mint({ address: sender, amount })
+    this.emitEvent("deposit", new FarmingEvent({ sender, amount }))
+  }
 
-        let { access, receive } = permissions.value
-        let accessIsNone = Provable.equal(Types.AuthRequired, access, Permissions.none())
-        let receiveIsNone = Provable.equal(Types.AuthRequired, receive, Permissions.none())
-        let updateAllowed = accessIsNone.and(receiveIsNone)
-
-        assert(
-            updateAllowed.or(permissions.isSome.not()),
-            "Can't change permissions for access or receive on token accounts"
-        )
-    }
-
-    @method
-    async transfer(from: PublicKey, to: PublicKey, amount: UInt64) {
-        from.equals(this.address).assertFalse("Can't transfer to/from the circulation account");
-        to.equals(this.address).assertFalse("Can't transfer to/from the circulation account");
-        this.internal.send({ from, to, amount })
-    }
-
-    @method
-    async deployStorage() {
-        const sender = this.sender.getUnconstrainedV2();
-
-        const newStorage = AccountUpdate.create(sender, this.deriveTokenId());
-        // Require this account didn't already exist
-        newStorage.account.isNew.requireEquals(Bool(true));
-
-        // set pool account vk and permission
-        newStorage.body.update.verificationKey = { isSome: Bool(true), value: { data: "", hash: Field(123n) } };
-        newStorage.body.update.permissions = {
-            isSome: Bool(true),
-            value: {
-                ...Permissions.default(),
-                // only proof to prevent signature owner to steal liquidity
-                access: Permissions.proof(),
-                setVerificationKey: Permissions.VerificationKey.proofDuringCurrentVersion(),
-                send: Permissions.proof(),
-                setPermissions: Permissions.impossible()
-            },
-        };
-
-        const poolAddress = this.pool.getAndRequireEquals();
-        const poolFields = poolAddress.toFields();
-
-        // init value
-        newStorage.body.update.appState = [
-            { isSome: Bool(true), value: poolFields[0] },
-            { isSome: Bool(true), value: poolFields[1] },
-            { isSome: Bool(true), value: Field(0) },
-            { isSome: Bool(true), value: Field(0) },
-            { isSome: Bool(true), value: Field(0) },
-            { isSome: Bool(true), value: Field(0) },
-            { isSome: Bool(true), value: Field(0) },
-            { isSome: Bool(true), value: Field(0) },
-        ];
-
-    }
-
-    @method
-    async checkPool(poolAddress: PublicKey) {
-        this.pool.requireEquals(poolAddress);
-    }
-
-    @method
-    async deposit(amount: UInt64) {
-        const sender = this.sender.getAndRequireSignatureV2();
-        const newStorage = new FarmStorage(sender, this.deriveTokenId());
-        await newStorage.deposit(amount);
-    }
-
+  @method
+  async withdraw(amount: UInt64) {
+    const sender = this.sender.getUnconstrainedV2()
+    this.send({ to: sender, amount })
+    this.internal.burn({ address: sender, amount })
+    this.emitEvent("withdraw", new FarmingEvent({ sender, amount }))
+  }
 }
---
+++ contracts/src/farming/FarmReward.ts
@@ -0,0 +1,113 @@
+import {
+  Account,
+  AccountUpdate,
+  AccountUpdateForest,
+  assert,
+  Bool,
+  CircuitString,
+  DeployArgs,
+  Field,
+  Int64,
+  MerkleWitness,
+  method,
+  Permissions,
+  Poseidon,
+  Provable,
+  PublicKey,
+  Reducer,
+  SmartContract,
+  State,
+  state,
+  Struct,
+  TokenContractV2,
+  TokenId,
+  Types,
+  UInt64,
+  VerificationKey
+} from "o1js"
+import { BalanceChangeEvent, mulDiv, Pool, PoolTokenHolder } from "../indexpool.js"
+
+export interface FarmingDeployProps extends Exclude<DeployArgs, undefined> {
+  pool: PublicKey
+  owner: PublicKey
+}
+
+export class ClaimEvent extends Struct({
+  user: PublicKey,
+  amount: UInt64
+}) {
+  constructor(value: {
+    user: PublicKey
+    amount: UInt64
+  }) {
+    super(value)
+  }
+}
+
+// support 65536 different depositor
+export class FarmMerkleWitness extends MerkleWitness(65536) { }
+
+/**
+ * Farm contract
+ */
+export class FarmReward extends TokenContractV2 {
+  @state(PublicKey)
+  owner = State<PublicKey>()
+  @state(Field)
+  merkleRoot = State<Field>()
+
+  events = {
+    upgrade: Field,
+    claim: ClaimEvent
+  }
+
+  async deploy(args: FarmingDeployProps) {
+    await super.deploy()
+
+    args.pool.isEmpty().assertFalse("Pool empty")
+    args.owner.isEmpty().assertFalse("Owner empty")
+
+    this.owner.set(args.owner)
+
+    let permissions = Permissions.default()
+    permissions.access = Permissions.proof()
+    permissions.setPermissions = Permissions.impossible()
+    permissions.setVerificationKey = Permissions.VerificationKey.proofDuringCurrentVersion()
+    this.account.permissions.set(permissions)
+  }
+
+  /** Approve `AccountUpdate`s that have been created outside of the token contract.
+   *
+   * @argument {AccountUpdateForest} updates - The `AccountUpdate`s to approve. Note that the forest size is limited by the base token contract, @see TokenContractV2.MAX_ACCOUNT_UPDATES The current limit is 9.
+   */
+  @method
+  async approveBase(updates: AccountUpdateForest): Promise<void> {
+    Bool(false).assertTrue("You can't manage the token")
+  }
+
+  @method
+  async withdrawReward(amount: UInt64, path: FarmMerkleWitness) {
+    const sender = this.sender.getAndRequireSignatureV2()
+    const senderBalance = AccountUpdate.create(sender, this.deriveTokenId())
+    senderBalance.account.balance.requireEquals(UInt64.zero)
+    const fieldSender = sender.toFields()
+    const hash = Poseidon.hash([fieldSender[0], fieldSender[1], amount.value])
+    const root = this.merkleRoot.getAndRequireEquals()
+    path.calculateRoot(hash).assertEquals(root, "Invalid request")
+    this.send({ to: sender, amount: amount })
+
+    // to prevent double withdraw we mint one token once a user withdraw
+    this.internal.mint({ address: senderBalance, amount: UInt64.one })
+    this.emitEvent("claim", new ClaimEvent({ user: sender, amount }))
+  }
+
+  @method
+  async withdrawDust() {
+    const sender = this.sender.getAndRequireSignatureV2()
+    this.owner.requireEquals(sender)
+    // only owner can winthdraw
+    const accountBalance = this.account.balance.getAndRequireEquals()
+    this.send({ to: sender, amount: accountBalance })
+    this.emitEvent("claim", new ClaimEvent({ user: sender, amount: accountBalance }))
+  }
+}
