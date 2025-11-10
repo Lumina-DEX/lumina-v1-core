786a8b14950548c0919f87d8fd76f262f90a411d
Youtpout <EddyBoughioul@hotmail.fr>
2025-11-10 11:48:36 +0100

    replace let by const in pool factory

--- contracts/src/pool/PoolFactory.ts
+++ contracts/src/pool/PoolFactory.ts
@@ -153,7 +153,7 @@ export class PoolFactory extends TokenContract implements PoolFactoryBase {
         this.protocol.set(args.protocol);
         this.delegator.set(args.delegator);
 
-        let permissions = Permissions.default();
+        const permissions = Permissions.default();
         permissions.access = Permissions.proof();
         permissions.setPermissions = Permissions.impossible();
         permissions.setVerificationKey = Permissions.VerificationKey.proofDuringCurrentVersion();
@@ -325,7 +325,7 @@ export class PoolFactory extends TokenContract implements PoolFactoryBase {
     }
 
     private async createAccounts(newAccount: PublicKey, token: PublicKey, token0: PublicKey, token1: PublicKey, signer: PublicKey, signature: Signature, path: MerkleMapWitness, right: Field, isTokenPool: boolean) {
-        let tokenAccount = AccountUpdate.create(token, this.deriveTokenId());
+        const tokenAccount = AccountUpdate.create(token, this.deriveTokenId());
         // if the balance is not zero, so a pool already exist for this token
         tokenAccount.account.balance.requireEquals(UInt64.zero);
 
@@ -400,11 +400,11 @@ export class PoolFactory extends TokenContract implements PoolFactoryBase {
     }
 
     private createState(token0: PublicKey, token1: PublicKey): { isSome: Bool; value: Field; }[] {
-        let token0Fields = token0.toFields();
-        let token1Fields = token1.toFields();
-        let poolFactory = this.address.toFields();
-        let protocol = this.protocol.getAndRequireEquals();
-        let protocolFields = protocol.toFields();
+        const token0Fields = token0.toFields();
+        const token1Fields = token1.toFields();
+        const poolFactory = this.address.toFields();
+        const protocol = this.protocol.getAndRequireEquals();
+        const protocolFields = protocol.toFields();
 
         return [
             { isSome: Bool(true), value: token0Fields[0] },
